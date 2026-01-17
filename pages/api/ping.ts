361d7895db24bd6a8ee99c156caec752b3157ab5
nymfluence <grkf696hxn@privaterelay.appleid.com>
2026-01-17 14:28:44 +0000

    Delete pages directory

--- pages/api/ping.ts
+++
@@ -1,8 +0,0 @@
-export const config = { runtime: "edge" };
-
-export default function handler() {
-  return new Response("pong", {
-    status: 200,
-    headers: { "content-type": "text/plain; charset=utf-8" }
-  });
-}
